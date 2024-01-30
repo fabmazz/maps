7afb0e28c9460fa65e5743a195ca1e631865e286
Garret <93139729+go2garret@users.noreply.github.com>
2024-01-30 01:37:35 -0500

    Update mapSources.js

--- src/mapSources.js
+++ src/mapSources.js
@@ -1,22 +1,19 @@
-export class MapSources {
-    // Define the map syle (OpenStreetMap raster tiles)
-  const openStreetMap = {
-    "version": 8,
-  	"sources": {
-      "osm": {
-  			"type": "raster",
-  			"tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
-  			"tileSize": 256,
-        "attribution": "&copy; OpenStreetMap Contributors",
-        "maxzoom": 19
-      }
-    },
-    "layers": [
-      {
-        "id": "osm",
-        "type": "raster",
-        "source": "osm" // This must match the source key above
-      }
-    ]
-  };
-}
+export const openStreetMap = {
+  "version": 8,
+  "sources": {
+    "osm": {
+      "type": "raster",
+      "tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+      "tileSize": 256,
+      "attribution": "&copy; OpenStreetMap Contributors",
+      "maxzoom": 19
+    }
+  },
+  "layers": [
+    {
+      "id": "osm",
+      "type": "raster",
+      "source": "osm" // This must match the source key above
+    }
+  ]
+};
